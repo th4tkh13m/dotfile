d3063a48785bad572362abaaa1bb0c4526f8adb5
th4tkh13m <khiemtonthat@gmail.com>
2021-11-02 08:23:19 +0700

    Finish software

--- install.sh
+++ install.sh
@@ -9,35 +9,54 @@ mkdir ~/dotfile_downloads
 curl --output ~/dotfile_downloads/dwm-6.2.tar.gz http://dl.suckless.org/dwm/dwm-6.2.tar.gz
 tar -zxvf ~/dotfile_downloads/dwm-6.2.tar.gz -C ~/dotfile_downloads
 cp ./software/dwm-6.2/dwm-fullscreen-6.2.diff ./software/dwm-6.2/config.h  ~/dotfile_downloads/dwm-6.2/
-# cd ~/dotfile_downloads/
-patch --merge -i ~/dotfile_downloads/dwm-6.2/dwm-fullscreen-6.2.diff
-make clean -C ~/dotfile_downloads/dwm-6.2
-sudo make install -C ~/dotfile_downloads/dwm-6.2
+cd ~/dotfile_downloads/
+patch -i dwm-fullscreen-6.2.diff
+make clean 
+sudo make install
+cd -
 
 # INSTALL st
 curl --output ~/dotfile_downloads/st-0.8.4.tar.gz http://dl.suckless.org/st/st-0.8.4.tar.gz
 tar -zxvf ~/dotfile_downloads/st-0.8.4.tar.gz -C ~/dotfile_downloads
 cp ./software/st-0.8.4/st-scrollback-0.8.4.diff ./software/st-0.8.4/config.h  ~/dotfile_downloads/st-0.8.4/
-patch --merge -i ~/dotfile_downloads/st-0.8.4/st-scrollback-0.8.4.diff
-make clean -C ~/dotfile_downloads/st-0.8.4
-sudo make install -C ~/dotfile_downloads/st-0.8.4
+cd ~/dotfile_downloads/st-0.8.4/
+patch -i st-scrollback-0.8.4.diff
+make clean
+sudo make install
+cd -
 
 # INSTALL dmenu
 curl --output ~/dotfile_downloads/dmenu-5.0.tar.gz http://dl.suckless.org/tools/dmenu-5.0.tar.gz
 tar -zxvf ~/dotfile_downloads/dmenu-5.0.tar.gz -C ~/dotfile_downloads
 cp ./software/dmenu-5.0/config.h  ~/dotfile_downloads/dmenu-5.0/
-make clean -C ~/dotfile_downloads/dmenu-5.0
-sudo make install -C ~/dotfile_downloads/dmenu-5.0
+cd ~/dotfile_downloads/dmenu-5.0
+make clean
+sudo make install
+cd -
 
 # INSTALL slock
 curl --output ~/dotfile_downloads/slock-1.4.tar.gz http://dl.suckless.org/tools/slock-1.4.tar.gz
 tar -zxvf ~/dotfile_downloads/slock-1.4.tar.gz -C ~/dotfile_downloads
 cp ./software/slock-1.4/config.h  ~/dotfile_downloads/slock-1.4/
-make clean -C ~/dotfile_downloads/slock-1.4
-sudo make install -C ~/dotfile_downloads/slock-1.4
+cd ~/dotfile_downloads/slock-1.4/
+make clean
+sudo make install
+cd -
 
 # INSTALL rover file manager
 git clone --depth 1 https://github.com/lecram/rover.git ~/dotfile_downloads/rover/
+cd ~/dotfile_downloads/rover/
+make clean
+sudo make install
+cd -
 
+# INSTALL PROGRAMMING LANGUAGES
+
+## INSTALL JAVA
+curl --output ~/dotfile_downloads/jdk17.tar.gz https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+tar -zxvf ~/dotfile_downloads/jdk17.tar.gz -C ~/dotfile_downloads/
+mkdir ~/Java
+mv ~/dotfile_downloads/jdk-17.0.1 /Java/
+echo 
 
 
